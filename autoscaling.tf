7243238d760741614dd48fd6dd29be503a6900a8
Julian Nonino <noninojulian@outlook.com>
2020-05-06 15:46:38 +0100

    Replacing comments

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,6 +1,6 @@
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # AWS ECS Auto Scale Role
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 resource "aws_iam_role" "ecs_autoscale_role" {
   name               = "${var.name_preffix}-ecs-autoscale-role"
   assume_role_policy = file("${path.module}/files/iam/ecs_autoscale_iam_role.json")
@@ -14,9 +14,9 @@ resource "aws_iam_role_policy" "ecs_autoscale_role_policy" {
   )
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # AWS Auto Scaling - CloudWatch Alarm CPU High
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 resource "aws_cloudwatch_metric_alarm" "cpu_high" {
   alarm_name          = "${var.name_preffix}-cpu-high"
   comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -33,9 +33,9 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high" {
   alarm_actions = [aws_appautoscaling_policy.scale_up_policy.arn]
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # AWS Auto Scaling - CloudWatch Alarm CPU Low
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 resource "aws_cloudwatch_metric_alarm" "cpu_low" {
   alarm_name          = "${var.name_preffix}-cpu-low"
   comparison_operator = "LessThanOrEqualToThreshold"
@@ -52,9 +52,9 @@ resource "aws_cloudwatch_metric_alarm" "cpu_low" {
   alarm_actions = [aws_appautoscaling_policy.scale_down_policy.arn]
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # AWS Auto Scaling - Scaling Up Policy
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 resource "aws_appautoscaling_policy" "scale_up_policy" {
   name               = "${var.name_preffix}-scale-up-policy"
   depends_on         = [aws_appautoscaling_target.scale_target]
@@ -72,9 +72,9 @@ resource "aws_appautoscaling_policy" "scale_up_policy" {
   }
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # AWS Auto Scaling - Scaling Down Policy
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 resource "aws_appautoscaling_policy" "scale_down_policy" {
   name               = "${var.name_preffix}-scale-down-policy"
   depends_on         = [aws_appautoscaling_target.scale_target]
@@ -92,9 +92,9 @@ resource "aws_appautoscaling_policy" "scale_down_policy" {
   }
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # AWS Auto Scaling - Scaling Target
-# ---------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 resource "aws_appautoscaling_target" "scale_target" {
   service_namespace  = "ecs"
   resource_id        = "service/${var.ecs_cluster_name}/${aws_ecs_service.service.name}"
