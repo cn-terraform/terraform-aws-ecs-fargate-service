81ed1d61f8ad2c0db04c2d9bdb7b731325a1fadb
Julian Nonino <noninojulian@gmail.com>
2020-11-26 18:45:50 +0000

    Updating task definition module

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -16,7 +16,7 @@ module "base-network" {
 
 module "td" {
   source          = "cn-terraform/ecs-fargate-task-definition/aws"
-  version         = "1.0.16"
+  version         = "1.0.17"
   name_prefix     = "test-td"
   container_image = "ubuntu"
   container_name  = "test"
