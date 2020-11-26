9261b3b56046b914b5791cd96ce903aee71c1fb0
Julian Nonino <noninojulian@gmail.com>
2020-11-26 17:37:16 +0000

    Adding outputs

--- outputs.tf
+++ outputs.tf
@@ -44,3 +44,129 @@ output "ecs_tasks_sg_description" {
   value       = aws_security_group.ecs_tasks_sg.description
 }
 
+#------------------------------------------------------------------------------
+# APPLICATION LOAD BALANCER
+#------------------------------------------------------------------------------
+output "aws_lb_lb_id" {
+  description = "The ARN of the load balancer (matches arn)."
+  value       = module.ecs-alb.aws_lb_lb_id
+}
+
+output "aws_lb_lb_arn" {
+  description = "The ARN of the load balancer (matches id)."
+  value       = module.ecs-alb.aws_lb_lb_arn
+}
+
+output "aws_lb_lb_arn_suffix" {
+  description = "The ARN suffix for use with CloudWatch Metrics."
+  value       = module.ecs-alb.aws_lb_lb_arn_suffix
+}
+
+output "aws_lb_lb_dns_name" {
+  description = "The DNS name of the load balancer."
+  value       = module.ecs-alb.aws_lb_lb_dns_name
+}
+
+output "aws_lb_lb_zone_id" {
+  description = "The canonical hosted zone ID of the load balancer (to be used in a Route 53 Alias record)."
+  value       = module.ecs-alb.aws_lb_lb_zone_id
+}
+
+#------------------------------------------------------------------------------
+# ACCESS CONTROL TO APPLICATION LOAD BALANCER
+#------------------------------------------------------------------------------
+output "aws_security_group_lb_access_sg_id" {
+  description = "The ID of the security group"
+  value       = module.ecs-alb.aws_security_group_lb_access_sg_id
+}
+
+output "aws_security_group_lb_access_sg_arn" {
+  description = "The ARN of the security group"
+  value       = module.ecs-alb.aws_security_group_lb_access_sg_arn
+}
+
+output "aws_security_group_lb_access_sg_vpc_id" {
+  description = "The VPC ID."
+  value       = module.ecs-alb.aws_security_group_lb_access_sg_vpc_id
+}
+
+output "aws_security_group_lb_access_sg_owner_id" {
+  description = "The owner ID."
+  value       = module.ecs-alb.aws_security_group_lb_access_sg_owner_id
+}
+
+output "aws_security_group_lb_access_sg_name" {
+  description = "The name of the security group"
+  value       = module.ecs-alb.aws_security_group_lb_access_sg_name
+}
+
+output "aws_security_group_lb_access_sg_description" {
+  description = "The description of the security group"
+  value       = module.ecs-alb.aws_security_group_lb_access_sg_description
+}
+
+output "aws_security_group_lb_access_sg_ingress" {
+  description = "The ingress rules."
+  value       = module.ecs-alb.aws_security_group_lb_access_sg_ingress
+}
+
+output "aws_security_group_lb_access_sg_egress" {
+  description = "The egress rules."
+  value       = module.ecs-alb.aws_security_group_lb_access_sg_egress
+}
+
+#------------------------------------------------------------------------------
+# AWS LOAD BALANCER - Target Groups
+#------------------------------------------------------------------------------
+output "lb_http_tgs_ids" {
+  description = "List of HTTP Target Groups IDs"
+  value       = module.ecs-alb.lb_http_tgs_ids
+}
+
+output "lb_http_tgs_arns" {
+  description = "List of HTTP Target Groups ARNs"
+  value       = module.ecs-alb.lb_http_tgs_arns
+}
+
+output "lb_http_tgs_names" {
+  description = "List of HTTP Target Groups Names"
+  value       = module.ecs-alb.lb_http_tgs_names
+}
+
+output "lb_https_tgs_ids" {
+  description = "List of HTTPS Target Groups IDs"
+  value       = module.ecs-alb.lb_https_tgs_ids
+}
+
+output "lb_https_tgs_arns" {
+  description = "List of HTTPS Target Groups ARNs"
+  value       = module.ecs-alb.lb_https_tgs_arns
+}
+
+output "lb_https_tgs_names" {
+  description = "List of HTTPS Target Groups Names"
+  value       = module.ecs-alb.lb_https_tgs_names
+}
+
+#------------------------------------------------------------------------------
+# AWS LOAD BALANCER - Listeners
+#------------------------------------------------------------------------------
+output "lb_http_listeners_ids" {
+  description = "List of HTTP Listeners IDs"
+  value       = module.ecs-alb.lb_http_listeners_ids
+}
+
+output "lb_http_listeners_arns" {
+  description = "List of HTTP Listeners ARNs"
+  value       = module.ecs-alb.lb_http_listeners_arns
+}
+
+output "lb_https_listeners_ids" {
+  description = "List of HTTPS Listeners IDs"
+  value       = module.ecs-alb.lb_https_listeners_ids
+}
+
+output "lb_https_listeners_arns" {
+  description = "List of HTTPS Listeners ARNs"
+  value       = module.ecs-alb.lb_https_listeners_arns
+}
