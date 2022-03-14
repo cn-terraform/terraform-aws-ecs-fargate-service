c624e140f00dcff4f283cb2d4dfb831f0e13fc06
Justin Myers <jcm112388@gmail.com>
2022-03-14 16:21:35 -0600

    Add option to enable ECS Exec

--- main.tf
+++ main.tf
@@ -64,6 +64,7 @@ resource "aws_ecs_service" "service" {
   deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent
   desired_count                      = var.desired_count
   enable_ecs_managed_tags            = var.enable_ecs_managed_tags
+  enable_execute_command             = var.enable_execute_command
   health_check_grace_period_seconds  = var.health_check_grace_period_seconds
   launch_type                        = "FARGATE"
   force_new_deployment               = var.force_new_deployment
