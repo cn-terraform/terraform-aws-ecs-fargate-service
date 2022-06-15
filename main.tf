aa025c6913a3cc4fa3320c6da557373b53cf860e
Yang, Bo <pop.atry@gmail.com>
2022-06-15 15:14:24 -0700

    Avoid duplicated security rules (fix #23)

--- main.tf
+++ main.tf
@@ -160,18 +160,8 @@ resource "aws_security_group_rule" "egress" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "ingress_through_http" {
-  for_each                 = toset(module.ecs-alb.lb_http_tgs_ports)
-  security_group_id        = aws_security_group.ecs_tasks_sg.id
-  type                     = "ingress"
-  from_port                = each.key
-  to_port                  = each.key
-  protocol                 = "tcp"
-  source_security_group_id = module.ecs-alb.aws_security_group_lb_access_sg_id
-}
-
-resource "aws_security_group_rule" "ingress_through_https" {
-  for_each                 = toset(module.ecs-alb.lb_https_tgs_ports)
+resource "aws_security_group_rule" "ingress_through_http_and_https" {
+  for_each                 = toset(concat(module.ecs-alb.lb_https_tgs_ports, module.ecs-alb.lb_http_tgs_ports))
   security_group_id        = aws_security_group.ecs_tasks_sg.id
   type                     = "ingress"
   from_port                = each.key
